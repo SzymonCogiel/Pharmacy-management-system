d5259857bad28eabf988a1f85025184be02347df
Szymon Cogiel <szymi130801@gmail.com>
2022-11-14 11:23:59 +0100

    mock log/reg

--- backend/pharamacy_system/system/views.py
+++ backend/pharamacy_system/system/views.py
@@ -3,13 +3,12 @@ from django.http import HttpResponse
 import json
 import matplotlib.pyplot as plt
 from PIL import Image
+from rest_framework.decorators import action
 
 class TestAPIView(APIView):
+
     @staticmethod
     def get(request):
-        # x = "test"
-        # response = str(json.dumps(str(x)))
-        # return HttpResponse(response, content_type="text/plain")
         plt.plot([1, 2, 3, 4])
         plt.ylabel('some numbers')
         plt.savefig("output.jpg")
@@ -21,3 +20,33 @@ class TestAPIView(APIView):
             response = HttpResponse(content_type="image/jpeg")
             red.save(response, "JPEG")
             return response
+
+
+class LogView(APIView):
+
+    @action(detail=False, methods=['get'])
+    def get(self, request):
+
+        # mockup until we have the database
+        mock_users = {'szymon': 'password'}
+        login = request.GET.get('login', None)
+        password = request.GET.get('password', None)
+
+        if mock_users[login] == password:
+            response = str(json.dumps(str("ok")))
+        else:
+            response = str(json.dumps(str("denial")))
+
+        return HttpResponse(response, content_type="text/plain")
+
+
+class RegisterView(APIView):
+
+    @action(detail=False, methods=['get'])
+    def post(self, request):
+        mail = request.GET.get('mail', None)
+        login = request.GET.get('login', None)
+        password = request.GET.get('password', None)
+        print(password)
+        response = str(json.dumps(str(login)))
+        return HttpResponse(response, content_type="text/plain")
