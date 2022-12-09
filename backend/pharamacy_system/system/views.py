58dfeb48f41226c27ed7a6cafa3069b8c94ce263
JoannaPapros <jpapros@student.agh.edu.pl>
2022-12-09 10:20:54 +0100

    PA-10 - Recepty gotowe

--- backend/pharamacy_system/system/views.py
+++ backend/pharamacy_system/system/views.py
@@ -100,6 +100,7 @@ class StockStatusView(APIView):
         drugname = request.GET.get('drugname')
         price = request.GET.get('price')
         amount = request.GET.get('amount')
+        prescription = request.GET.get('prescription')
 
         drugInfo = DrugsInfo.objects.all()
 
@@ -118,6 +119,11 @@ class StockStatusView(APIView):
             pass
         elif amount:
             drugInfo = drugInfo.filter(amount=amount)
+
+        if prescription == "undefined":
+            pass
+        elif amount:
+            drugInfo = drugInfo.filter(prescription=prescription)
 
         serializer = DrugsInfoSerializer(drugInfo, many=True)
         return Response(serializer.data)
--- frontend/src/dostepne_leki.js
+++ frontend/src/dostepne_leki.js
@@ -79,19 +79,20 @@ function Dostepne_leki() {
                         onChange={(e) => setName(e.target.value)} />
             </Form.Group>
                 <input type="hidden" name="ie" value="utf-8" />
-                <input type="radio" name="lr" value="" id="lr0" checked /> <label for="lr0">Sortuj według miejsca</label>
-                <input type="radio" name="lr" value="lang_pl" id="lr1" /> <label for="lr1">Sortuj według liczby sztuk</label>
+                {/*<input type="radio" name="lr" value="" id="lr0" checked /> <label for="lr0">Sortuj według miejsca</label>
+                <input type="radio" name="lr" value="lang_pl" id="lr1" /> <label for="lr1">Sortuj według liczby sztuk</label>*/}
             </Form>
             <br />
             <br />
             <table>
               {/* tu w tabeli do dorobienia jeszcze recepta i miejsce położenia(jakis randomowy zlep typu M-13 jako półka) + sortowania */}
-            <tr><th>Nazwa</th><th>Liczba sztuk</th><th>Cena</th></tr>
+            <tr><th>Nazwa</th><th>Liczba sztuk</th><th>Cena</th><th>Recepta</th></tr>
              {data.map((item, index) => (
              <tr key={index}>
             <td>{item.drugname}</td>
             <td>{item.amount}</td>
             <td>{item.price}</td>
+            <td>{item.prescription}</td>
             </tr>
             ))}
             </table>         
